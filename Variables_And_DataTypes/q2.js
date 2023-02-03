329bc3086ba5bf1a82188e31deee254b25ffb3fe
ManishGupt5 <Manishgupt9211@gmail.com>
2023-02-03 14:30:07 +0530

    Creating Folder

---
+++ Variables_And_DataTypes/q2.js
@@ -0,0 +1,6 @@
+var name="Manish"
+console.log(name);
+name="My Father's Name"
+console.log(name);
+name="My Mother's Name"
+console.log(name);
---
+++ Variables_And_DataTypes/q4.js
@@ -0,0 +1,17 @@
+var name, school, grade, section, rollno,subject1,subject2,subject3
+name="Ram" 
+  school="BHU"
+    grade="A" 
+      section="B" 
+        rollno=15 
+          subject1=80
+            subject2=90
+              subject3=95
+console.log("Name: "+name)
+console.log("school: "+school)
+  console.log("Grade: "+grade)
+    console.log("section: "+section)
+console.log("roll no: "+rollno)
+console.log("subject1: "+subject1)
+console.log("subject2: "+subject2)
+console.log("subject3: "+subject3)
